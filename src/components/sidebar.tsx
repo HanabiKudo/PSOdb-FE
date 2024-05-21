b154c78813d208a38d0a3ab628ef724ac4b14770
HanabiKudo <retro.mendozaa@gmail.com>
2024-05-21 12:06:48 -0600

    edited side bar to have toggle

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,5 @@
 // components/FloatingNav.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 const navStyle: React.CSSProperties = {
@@ -12,6 +12,9 @@ const navStyle: React.CSSProperties = {
   padding: '20px',
   boxShadow: '2px 0 5px rgba(0,0,0,0.1)',
   zIndex: 999,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
 };
 
 const categoryStyle: React.CSSProperties = {
@@ -24,7 +27,7 @@ const ulStyle: React.CSSProperties = {
   listStyle: 'none',
   padding: 0,
   margin: 0,
-  marginBottom: '50px',
+  marginBottom: '20px',
 };
 
 const liStyle: React.CSSProperties = {
@@ -38,38 +41,114 @@ const linkStyle: React.CSSProperties = {
   fontSize: '14px',
 };
 
+const toggleContainerStyle: React.CSSProperties = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  padding: '10px 0',
+  borderTop: '1px solid #444',
+  color: 'white',
+  marginBottom: '30px',
+};
+
+const toggleLabelStyle: React.CSSProperties = {
+  fontSize: '14px',
+  marginRight: '10px',
+};
+
+const toggleSwitchStyle: React.CSSProperties = {
+  position: 'relative',
+  display: 'inline-block',
+  width: '40px',
+  height: '20px',
+};
+
+const toggleSliderStyle: React.CSSProperties = {
+  position: 'absolute',
+  cursor: 'pointer',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: '#ccc',
+  transition: '.4s',
+  borderRadius: '20px',
+};
+
+const toggleSliderBeforeStyle: React.CSSProperties = {
+  position: 'absolute',
+  content: '""',
+  height: '14px',
+  width: '14px',
+  borderRadius: '50%',
+  backgroundColor: 'white',
+  transition: '.4s',
+  left: '3px',
+  bottom: '3px',
+};
+
+const toggleSliderCheckedStyle: React.CSSProperties = {
+  backgroundColor: '#2196F3',
+};
+
+const toggleSliderCheckedBeforeStyle: React.CSSProperties = {
+  transform: 'translateX(20px)',
+};
+
 const FloatingNav: React.FC = () => {
+  const [toggleState, setToggleState] = useState<boolean>(false);
+
+  const handleToggleChange = () => {
+    setToggleState(!toggleState);
+  };
+
   return (
     <nav style={navStyle}> 
       <div>
-        <h3 style={categoryStyle}>Player</h3>
-        <ul style={ulStyle}>
-          <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>SectionIDs</a></Link></li>
-          <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>Classes</a></Link></li>
-          <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>Name Calc</a></Link></li> 
-        </ul>
+        <div>
+          <h3 style={categoryStyle}>Player</h3>
+          <ul style={ulStyle}>
+            <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>SectionIDs</a></Link></li>
+            <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>Classes</a></Link></li>
+            <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>Name Calc</a></Link></li> 
+          </ul>
+        </div>
+
+        <div>
+          <h3 style={categoryStyle}>Gear</h3>
+          <ul style={ulStyle}>
+            <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>Weapons</a></Link></li>
+            <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>Frames</a></Link></li>
+            <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>Barriers</a></Link></li> 
+            <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>Units</a></Link></li>  
+            <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>Mags</a></Link></li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 style={categoryStyle}>Zones</h3>
+          <ul style={ulStyle}>
+            <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>Monsters</a></Link></li>
+            <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>Maps</a></Link></li>
+          </ul>
+        </div>
       </div>
 
-      <div>
-        <h3 style={categoryStyle}>Gear</h3>
-        <ul style={ulStyle}>
-          <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>Weapons</a></Link></li>
-          <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>Frames</a></Link></li>
-          <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>Barriers</a></Link></li> 
-          <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>Units</a></Link></li>  
-          <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>Mags</a></Link></li>
-        </ul>
+      <div style={toggleContainerStyle}>
+        <span style={toggleLabelStyle}>{toggleState ? 'Ragol' : 'Vanilla'}</span>
+        <label style={toggleSwitchStyle}>
+          <input type="checkbox" checked={toggleState} onChange={handleToggleChange} style={{ display: 'none' }} />
+          <span style={{ 
+            ...toggleSliderStyle, 
+            ...(toggleState ? toggleSliderCheckedStyle : {}),
+          }}>
+            <span style={{ 
+              ...toggleSliderBeforeStyle, 
+              ...(toggleState ? toggleSliderCheckedBeforeStyle : {}),
+            }}></span>
+          </span>
+        </label>
       </div>
-
-      <div>
-        <h3 style={categoryStyle}>Zones</h3>
-        <ul style={ulStyle}>
-          <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>Monsters</a></Link></li>
-          <li style={liStyle}><Link legacyBehavior href="/"><a style={linkStyle}>Maps</a></Link></li>
-        </ul>
-      </div>
-
-      
     </nav>
   );
 };
